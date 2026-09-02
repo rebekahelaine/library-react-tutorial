a9584d903767c0baefbe9992468af993db734750
Rebekah Alderfer <rebekah@titlesafe.tv>
2026-09-02 17:17:40 -0500

    completed Highlights section with Highlight ui component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './index.css';
 import Nav from './components/Nav.jsx'
+import Landing from './components/Landing.jsx'
+import Highlights from './components/Highlights.jsx'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
